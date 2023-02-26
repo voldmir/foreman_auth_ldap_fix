11f482b267a31ddb5c44dd9963d0c17c0914167d
Vladimir Savchenko <voldmir@mail.ru>
2023-02-26 17:11:02 +0700

    optimization code

--- app/controllers/concerns/foreman_auth_ldap_fix/usergroups_controller_ext.rb
+++ app/controllers/concerns/foreman_auth_ldap_fix/usergroups_controller_ext.rb
@@ -1,22 +1,10 @@
 module ForemanAuthLdapFix
   module UsergroupsControllerExt
     extend ActiveSupport::Concern
-    include Foreman::Controller::Parameters::Taxonomix
-
     class_methods do
       def usergroup_params_filter
-        Foreman::ParameterFilter.new(::Usergroup).tap do |filter|
-          filter.permit :name,
-                        :external_usergroups_attributes => [external_usergroup_params_filter],
-                        :role_ids => [], :role_names => [],
-                        :user_ids => [], :user_names => [],
-                        :usergroup_ids => [], :usergroup_names => []
-
-          add_taxonomix_params_filter(filter)
-
-          filter.permit do |ctx|
-            ctx.permit :admin if User.current.try(:admin?) && (ctx.ui? || ctx.api?)
-          end
+        super.tap do |filter|
+          filter.permit :organization_ids => []
         end
       end
     end
